a490cf03e84f20b06fe7d942d2f0d7bddb3dbcc8
Shyam Kumar <skumar11@gmail.com>
2019-05-26 23:21:22 -0500

    add gradle support

---
+++ src/main/java/com/shyam/HelloWorldLocalTime.java
@@ -0,0 +1,13 @@
+package main.java.com.shyam;
+
+import org.joda.time.LocalTime;
+
+public class HelloWorldLocalTime {
+  public static void main(String[] args) {
+    LocalTime currentTime = new LocalTime();
+    System.out.println("The current local time is: " + currentTime);
+
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
+  }
+}
